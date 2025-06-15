e141c8f2c168711dea47f56bd3b304d30b98200f
Your Name <your.email@example.com>
2025-06-15 20:34:38 +0800

    Add TestComponent and story for deployment test

---
+++ src/components/TestComponent.stories.jsx
@@ -0,0 +1,9 @@
+import React from 'react';
+import TestComponent from './TestComponent';
+
+export default {
+  title: 'Components/TestComponent',
+  component: TestComponent,
+};
+
+export const Default = () => <TestComponent />; 
